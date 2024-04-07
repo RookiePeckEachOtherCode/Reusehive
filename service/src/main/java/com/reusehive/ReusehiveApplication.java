8f31590d2aaf6841858416396539831472119632
Beriholic <beriholic@petalmail.com>
2024-04-07 09:57:51 +0800

    add mapper dir

---
+++ service/src/main/java/com/reusehive/ReusehiveApplication.java
@@ -0,0 +1,13 @@
+package com.reusehive;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.reusehive.mapper")
+public class ReusehiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReusehiveApplication.class, args);
+    }
+}
