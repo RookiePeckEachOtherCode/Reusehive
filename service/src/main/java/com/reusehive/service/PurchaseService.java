4c19d1c443078cb873c69c79cd14113cfe5b9300
lingluosama <xiwulingluo@outlook.com>
2024-04-08 21:06:28 +0800

    PurchaseService

---
+++ service/src/main/java/com/reusehive/service/PurchaseService.java
@@ -0,0 +1,20 @@
+package com.reusehive.service;
+
+import com.reusehive.entity.database.PurchaseInfo;
+
+import java.util.List;
+
+public interface PurchaseService {
+    void CreatePurchase(Long user_id,Long item_id,Double price,Long login_id);
+
+    void PurchaseComplete(Long purchase_id);
+
+    List<PurchaseInfo> GetUserPurchaseList(Long user_id);
+
+    PurchaseInfo GetPurchaseInfoById(Long purchase_id);
+
+    void RemoveItem(Long item_id);
+
+    void CanclePurchase(Long purchase_id);
+
+}
