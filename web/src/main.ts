48f9656ea77f02e90b8b4bc1bc0649dba1e87442
Beriholic <96678707+Beriholic@users.noreply.github.com>
2024-04-12 17:09:27 +0800

    update frontend

--- web/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import ElementPlus from 'element-plus'
-import App from './App.vue'
-import router from "./router";
-import {createPinia} from "pinia";
-
-createApp(App).use(ElementPlus).use(router).use(createPinia).mount('#app')
